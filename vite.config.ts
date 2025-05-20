44315c4460b90c54b7221d3fa69364417894fe63
Jacopo Zagoli <poipoi.jacopo@hotmail.com>
2025-05-20 20:12:39 +0200

    flowbite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	test: {
 		workspace: [
 			{
