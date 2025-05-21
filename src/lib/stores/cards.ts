1fb7393619f74a91881084e061caf5fe7427a0cf
Jacopo Zagoli <poipoi.jacopo@hotmail.com>
2025-05-21 14:24:22 +0200

    added cards

---
+++ src/lib/stores/cards.ts
@@ -0,0 +1,4 @@
+export const rooms = ["Ingresso", "Sala del biliardo", "Cucina", "Sala da pranzo", "Soggiorno", "Camera da letto", "Studio", "Stanza da bagno", "Garage"];
+export const suspects = ["Plum", "Scarlet", "Green", "Mustard", "Peacock", "White"];
+export const weapons = ["Pistola", "Candeliere", "Corda", "Tubo di piombo", "Candeliere", "Chiave inglese"];
+export const cards = rooms.concat(suspects, weapons);
